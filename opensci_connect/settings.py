a25296f8bab62ee851668177ea22397ba99e128d
Far9999 <sadikalhanssah@gmail.com>
2023-10-07 09:58:22 +0000

    perform migrations to db

--- opensci_connect/settings.py
+++ opensci_connect/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "django_backend.urls"
+ROOT_URLCONF = "opensci_connect.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "django_backend.wsgi.application"
+WSGI_APPLICATION = "opensci_connect.wsgi.application"
 
 
 # Database
