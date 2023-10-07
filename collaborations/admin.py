57b1e13f048e4094e2d8354e8a963eadde365d8f
Far9999 <sadikalhanssah@gmail.com>
2023-10-07 09:17:40 +0000

    registered models for admin page

--- collaborations/admin.py
+++ collaborations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import UserProfile, Project, Message, UserProjectInteraction
+
+
+# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Project)
+admin.site.register(Message)
+admin.site.register(UserProjectInteraction)
