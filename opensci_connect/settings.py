0b8f571e5af47cfd6b70b1ee5d2e0d371bb47536
Bryan Tineo <brillant16.gcv@gmail.com>
2023-10-07 20:24:40 -0700

    front-end v1

--- opensci_connect/settings.py
+++ opensci_connect/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os #<--- for static folder
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+# Reflecting that the static directory is at the root level of the project.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+# The directory where Django will collect all static files using the `collectstatic` command.
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
